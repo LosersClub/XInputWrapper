fc0aa467b60ed3a97d16d4edf509920e9f1586f2
Kyle Bartz <kyle@unleashurgeek.com>
2018-10-13 21:07:38 -0700

    added a namespace to ControllerInput

--- ControllerInput/ControllerInput.cs
+++ ControllerInput/ControllerInput.cs
@@ -1,151 +1,172 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-class Imports
-{
-  internal const string DLLName = "XInputInterface";
+namespace XInputWrapper {
+  class Imports {
+    internal const string DLLName = "XInputInterface";
 
-  [DllImport(DLLName)]
-  public static extern uint XInputGetControllerState(uint userIndex, out ControllerState state);
-  [DllImport(DLLName)]
-  public static extern void XInputSetControllerState(uint userIndex, float leftMotorVib, float rightMotorVib);
-  [DllImport(DLLName)]
-  public static extern bool XInputControllerConnected(uint userIndex);
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct Thumbstick {
-  float x;
-  float y;
-
-  public bool X {
-    get { return x; }
+    [DllImport(DLLName)]
+    public static extern uint XInputGetControllerState(uint userIndex, out ControllerState state);
+    [DllImport(DLLName)]
+    public static extern void XInputSetControllerState(uint userIndex, float leftMotorVib, float rightMotorVib);
+    [DllImport(DLLName)]
+    public static extern bool XInputControllerConnected(uint userIndex);
   }
 
-  public bool Y {
-    get { return y; }
-  }
-}
+  [StructLayout(LayoutKind.Sequential)]
+  public struct Thumbstick {
+    float x;
+    float y;
 
-[StructLayout(LayoutKind.Sequential)]
-public struct DPad {
-  bool up;
-  bool right;
-  bool down;
-  bool left;
+    public bool X {
+      get { return x; }
+    }
 
-  public bool Up {
-    get { return up; }
+    public bool Y {
+      get { return y; }
+    }
   }
 
-  public bool Right {
-    get { return right; }
+  [StructLayout(LayoutKind.Sequential)]
+  public struct DPad {
+    bool up;
+    bool right;
+    bool down;
+    bool left;
+
+    public bool Up
+    {
+      get { return up; }
+    }
+
+    public bool Right
+    {
+      get { return right; }
+    }
+
+    public bool Down
+    {
+      get { return down; }
+    }
+
+    public bool Left
+    {
+      get { return up; }
+    }
   }
 
-  public bool Down {
-    get { return down; }
+  [StructLayout(LayoutKind.Sequential)]
+  public struct ControllerState {
+    bool isConnected;
+    bool a;
+    bool b;
+    bool x;
+    bool y;
+    bool start;
+    bool back;
+    bool rightBumper;
+    bool leftBumper;
+    bool rightThumbstickButton;
+    bool leftThumbstickButton;
+    DPad dPad;
+    float rightTrigger;
+    float leftTrigger;
+    Thumbstick rightThumbstick;
+    Thumbstick leftThumbstick;
+
+    public bool IsConnected
+    {
+      get { return isConnected; }
+    }
+
+    public bool A
+    {
+      get { return a; }
+    }
+
+    public bool B
+    {
+      get { return b; }
+    }
+
+    public bool X
+    {
+      get { return x; }
+    }
+
+    public bool Y
+    {
+      get { return y; }
+    }
+
+    public bool Start
+    {
+      get { return start; }
+    }
+
+    public bool Back
+    {
+      get { return back; }
+    }
+
+    public bool RightBumper
+    {
+      get { return rightBumper; }
+    }
+
+    public bool LeftBumper
+    {
+      get { return leftBumper; }
+    }
+
+    public bool RightThumbstickButton
+    {
+      get { return rightThumbstickButton; }
+    }
+
+    public bool LeftThumbstickButton
+    {
+      get { return leftThumbstickButton; }
+    }
+
+    public DPad DPad
+    {
+      get { return dPad; }
+    }
+
+    public float RightTrigger
+    {
+      get { return rightTrigger; }
+    }
+
+    public float LeftTrigger
+    {
+      get { return leftTrigger; }
+    }
+
+    public Thumbstick RightThumbstick
+    {
+      get { return rightThumbstick; }
+    }
+
+    public Thumbstick LeftThumbstick
+    {
+      get { return leftThumbstick; }
+    }
   }
 
-  public bool Left {
-    get { return up; }
-  }
-}
+  public class ControllerState {
+    public static ControllerState GetState(uint userIndex) {
+      ControllerState state;
+      return Imports.XInputGetControllerState(userIndex, out state);
+    }
 
-[StructLayout(LayoutKind.Sequential)]
-public struct ControllerState {
-  bool isConnected;
-  bool a;
-  bool b;
-  bool x;
-  bool y;
-  bool start;
-  bool back;
-  bool rightBumper;
-  bool leftBumper;
-  bool rightThumbstickButton;
-  bool leftThumbstickButton;
-  DPad dPad;
-  float rightTrigger;
-  float leftTrigger;
-  Thumbstick rightThumbstick;
-  Thumbstick leftThumbstick;
+    public static void SetState(uint userIndex, float leftMotorVib, float rightMotorVib) {
+      Imports.XInputSetControllerState(userIndex, leftMotorVib, rightMotorVib);
+    }
 
-  public bool IsConnected {
-    get { return isConnected; }
-  }
-
-  public bool A {
-    get { return a; }
-  }
-
-  public bool B {
-    get { return b; }
-  }
-
-  public bool X {
-    get { return x; }
-  }
-
-  public bool Y {
-    get { return y; }
-  }
-
-  public bool Start {
-    get { return start; }
-  }
-
-  public bool Back {
-    get { return back; }
-  }
-
-  public bool RightBumper {
-    get { return rightBumper; }
-  }
-
-  public bool LeftBumper {
-    get { return leftBumper; }
-  }
-
-  public bool RightThumbstickButton {
-    get { return rightThumbstickButton; }
-  }
-
-  public bool LeftThumbstickButton {
-    get { return leftThumbstickButton; }
-  }
-
-  public DPad DPad {
-    get { return dPad; }
-  }
-
-  public float RightTrigger {
-    get { return rightTrigger; }
-  }
-
-  public float LeftTrigger {
-    get { return leftTrigger; }
-  }
-
-  public Thumbstick RightThumbstick {
-    get { return rightThumbstick; }
-  }
-
-  public Thumbstick LeftThumbstick {
-    get { return leftThumbstick; }
-  }
-}
-
-public class ControllerState {
-  public static ControllerState GetState(uint userIndex) {
-    ControllerState state;
-    return Imports.XInputGetControllerState(userIndex, out state);
-  }
-
-  public static void SetState(uint userIndex, float leftMotorVib, float rightMotorVib) {
-    Imports.XInputSetControllerState(userIndex, leftMotorVib, rightMotorVib);
-  }
-
-  public static bool ControllerConnected(uint userIndex) {
-    return Imports.XInputControllerConnected(userIndex);
+    public static bool ControllerConnected(uint userIndex) {
+      return Imports.XInputControllerConnected(userIndex);
+    }
   }
 }
